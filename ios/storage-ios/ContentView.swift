556527d90a36b0798e162a5a76b9dafa6b7491c0
hamasaki <mfhama9910@icloud.com>
2023-04-14 10:51:08 +0900

    connect iphone

--- ios/storage-ios/ContentView.swift
+++ ios/storage-ios/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, worlad!")
+            Text("Connect My IphoneXs!")
         }
         .padding()
     }
