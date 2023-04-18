a9281905b4265cf664dcdf8a2a093f8e192d5e87
hamasaki <mfhama9910@icloud.com>
2023-04-19 00:26:40 +0900

    update db #12

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,5 @@
--- INSERT INTO Items (id, title, artist, price)
---     VALUES 
---         (1, "Blue Train", "John Coltrane", 5911),
---         (2, "Jeru", "Gerry Mulligan", 1726),
---         (3, "Sarah Vaughan and Clifford Brown", "Sarah Vaughan", 3920);
-
-INSERT INTO Bookmarks (url, title, description, tag_id)
+INSERT INTO Items (id, title, artist, price)
     VALUES 
-        ("http://url.first", "Blue Train", "John Coltrane", 1),
-        ("http://url.second", "Jeru", "Gerry Mulligan", 2),
-        ("http://url.third", "Sarah Vaughan and Clifford Brown", "Sarah Vaughan", 3);
+        (1, "Blue Train", "John Coltrane", 5911),
+        (2, "Jeru", "Gerry Mulligan", 1726),
+        (3, "Sarah Vaughan and Clifford Brown", "Sarah Vaughan", 3920);
