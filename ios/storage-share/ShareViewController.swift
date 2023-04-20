b1525a03adbf2bd0a3421b96d5d11761021f950e
hama404 <67415197+hama404@users.noreply.github.com>
2023-04-21 04:00:00 +0900

    Revert "Feature/url"

--- ios/storage-share/ShareViewController.swift
+++ ios/storage-share/ShareViewController.swift
@@ -9,33 +9,22 @@ import UIKit
 import Social
 
 class ShareViewController: SLComposeServiceViewController {
-  
-  override func isContentValid() -> Bool {
-    // Do validation of contentText and/or NSExtensionContext attachments here
-    return true
-  }
-  
-  override func didSelectPost() {
-    // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
 
-    guard let extensionItem: NSExtensionItem = extensionContext?.inputItems.first as? NSExtensionItem, let itemProviders = extensionItem.attachments else { return }
-    let identifier = "public.url"
-    let urlProvider = itemProviders.first(where: { $0.hasItemConformingToTypeIdentifier(identifier)})
-    urlProvider?.loadItem(forTypeIdentifier: identifier, options: nil, completionHandler: { [weak self] (item, error) in
-      guard let url = item as? URL else { return }
+    override func isContentValid() -> Bool {
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        return true
+    }
 
-      print("hello post!!", url)
-      createShareData(url: url)
+    override func didSelectPost() {
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    }
 
-    })
+    override func configurationItems() -> [Any]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
 
-    // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-    self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
-  }
-  
-  override func configurationItems() -> [Any]! {
-    // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-    return []
-  }
-  
 }
