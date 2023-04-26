292d3b13fb486131b6a65be07bcfedb25efe8935
hamasaki <mfhama9910@icloud.com>
2023-04-27 05:19:07 +0900

    call api from front #18

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "/api/": {
+        target: "https://fashion-app-z2zcp4g4ca-uw.a.run.app",
+      }
+    }
+  }
 })
